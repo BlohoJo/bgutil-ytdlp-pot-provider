4d33d6e03b799d49a31e601dee80107c992fba3c
BlohoJo <chase63790@gmail.com>
2025-10-28 09:48:41 -0400

    Comment out proxy parameter in getpot_bgutil_http.py
    Script doesn't want to get a token if a proxy is used in yt-dlp. So this keeps the proxy for yt-dlp, but will allow the POT server to connect directly.

--- yt_dlp_plugins/extractor/getpot_bgutil_http.py
+++ yt_dlp_plugins/extractor/getpot_bgutil_http.py
@@ -130,7 +130,7 @@ class BgUtilHTTPPTP(BgUtilPTPBase):
                         'content_binding': get_webpo_content_binding(request)[0],
                         'disable_innertube': disable_innertube,
                         'disable_tls_verification': not request.request_verify_tls,
-                        'proxy': request.request_proxy,
+                      # 'proxy': request.request_proxy,
                         'innertube_context': request.innertube_context,
                         'source_address': request.request_source_address,
                     }).encode(), headers={'Content-Type': 'application/json'},
